48d30f89b30c7a0016ff8fcf917cd7015e7ffc35
Rost Visotskiy <rotyslav@rambler.ru>
2018-01-24 09:24:48 +0200

    complete the project

---
+++ Multithreaded Applications/MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Multithreaded_Applications
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.path_to_folder = new System.Windows.Forms.TextBox();
+            this.select_folder = new System.Windows.Forms.Button();
+            this.extensions = new System.Windows.Forms.ComboBox();
+            this.search = new System.Windows.Forms.Button();
+            this.list_files = new System.Windows.Forms.ListBox();
+            this.count_files = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.en = new System.Windows.Forms.ToolStripMenuItem();
+            this.ru = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.SuspendLayout();
+            // 
+            // path_to_folder
+            // 
+            resources.ApplyResources(this.path_to_folder, "path_to_folder");
+            this.path_to_folder.ForeColor = System.Drawing.Color.Black;
+            this.path_to_folder.Name = "path_to_folder";
+            // 
+            // select_folder
+            // 
+            resources.ApplyResources(this.select_folder, "select_folder");
+            this.select_folder.Name = "select_folder";
+            this.select_folder.UseVisualStyleBackColor = true;
+            this.select_folder.Click += new System.EventHandler(this.select_folder_Click);
+            // 
+            // extensions
+            // 
+            resources.ApplyResources(this.extensions, "extensions");
+            this.extensions.FormattingEnabled = true;
+            this.extensions.Items.AddRange(new object[] {
+            resources.GetString("extensions.Items"),
+            resources.GetString("extensions.Items1"),
+            resources.GetString("extensions.Items2"),
+            resources.GetString("extensions.Items3"),
+            resources.GetString("extensions.Items4")});
+            this.extensions.Name = "extensions";
+            // 
+            // search
+            // 
+            resources.ApplyResources(this.search, "search");
+            this.search.Name = "search";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // list_files
+            // 
+            resources.ApplyResources(this.list_files, "list_files");
+            this.list_files.FormattingEnabled = true;
+            this.list_files.Name = "list_files";
+            // 
+            // count_files
+            // 
+            resources.ApplyResources(this.count_files, "count_files");
+            this.count_files.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.count_files.Name = "count_files";
+            // 
+            // stop
+            // 
+            resources.ApplyResources(this.stop, "stop");
+            this.stop.Name = "stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // en
+            // 
+            this.en.Name = "en";
+            resources.ApplyResources(this.en, "en");
+            // 
+            // ru
+            // 
+            this.ru.Name = "ru";
+            resources.ApplyResources(this.ru, "ru");
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            resources.ApplyResources(this.toolStripComboBox1, "toolStripComboBox1");
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.count_files);
+            this.Controls.Add(this.list_files);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.extensions);
+            this.Controls.Add(this.select_folder);
+            this.Controls.Add(this.path_to_folder);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox path_to_folder;
+        private System.Windows.Forms.ComboBox extensions;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.ListBox list_files;
+        private System.Windows.Forms.Label count_files;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button select_folder;
+        private System.Windows.Forms.ToolStripMenuItem en;
+        private System.Windows.Forms.ToolStripMenuItem ru;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+    }
+}
+
---
+++ Multithreaded Applications/MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Multithreaded_Applications
+{
+    struct InputeData
+    {
+        public string pach;
+        public string exp;
+    }
+
+    public partial class MainForm : Form
+    {
+        static private BackgroundWorker BackgroundWorker = null;  
+        static int file_count = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();         
+        }
+
+        private void select_folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog selecting_folder = new FolderBrowserDialog();
+            selecting_folder.Description = "Выберите папку для поиска файлов.";
+            if (selecting_folder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {               
+                path_to_folder.Text = selecting_folder.SelectedPath;
+            }
+        }
+
+        public static void EnumerateAllFiles(string pach, string exp, BackgroundWorker bw, DoWorkEventArgs e)
+        {
+            try
+            {
+                if (bw.CancellationPending)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    IEnumerable<string> files = null;
+                    try { files = Directory.EnumerateFiles(pach, exp); }
+                    catch { }
+
+                    if (files != null)
+                    {
+                        foreach (string file in files)
+                        {
+                            file_count++;
+                            BackgroundWorker.ReportProgress(file_count, file);
+                        }
+                    }
+                    IEnumerable<string> directories = null;
+                    try { directories = Directory.EnumerateDirectories(pach); }
+                    catch { }
+
+                    if (directories != null)
+                    {
+                        foreach (var file in directories)
+                        {
+                            EnumerateAllFiles(file, exp, bw, e);
+                        }
+                    }
+                    e.Result = file_count;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            const string caption = "Ошибка ввода данных файла!";
+            if (path_to_folder.Text.Length == 0)
+            {
+                string message = "Путь не задан.";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (!Directory.Exists(path_to_folder.Text))
+            {
+                string message = "Заданная папка не существует.";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (extensions.Text.Length == 0)
+            {
+                string message = "Не задан тип файлов.";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                file_count = 0;
+                list_files.Items.Clear();
+                search.Enabled = false;
+                InputeData IdDt;
+                IdDt.pach = path_to_folder.Text;
+                IdDt.exp = "*." + extensions.Text;
+
+                BackgroundWorker = new BackgroundWorker();
+                BackgroundWorker.WorkerReportsProgress = true;
+                BackgroundWorker.WorkerSupportsCancellation = true;
+                BackgroundWorker.DoWork += backgroundWorker1_DoWork;
+                BackgroundWorker.ProgressChanged += backgroundWorker1_ProgressChanged;
+                BackgroundWorker.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+                BackgroundWorker.RunWorkerAsync(IdDt);
+            }
+        }
+       
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = sender as BackgroundWorker;
+            InputeData value = (InputeData)e.Argument;
+            EnumerateAllFiles(value.pach, value.exp, bw, e);                 
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            string message_list = "Поиск завершон!";
+            if (e.Cancelled)
+            {
+                string message = "Поиск остановлен.";
+                MessageBox.Show(message, "Собщение!", MessageBoxButtons.OK, MessageBoxIcon.Stop);;
+            }
+            else if (e.Error != null)
+            {
+                string message = "Ошибка выполнения: " + e.Error;
+                MessageBox.Show(message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);      
+            }
+            else
+            {
+                string message = "Поиск закончен успешно. Найденных файлов - " + e.Result + ". ";
+                MessageBox.Show(message, "Собщение!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            list_files.Items.Add(message_list);
+            FindMyString(message_list);
+            search.Enabled = true;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            count_files.Text = "Количество найденных файлов: "+ e.ProgressPercentage.ToString();
+            list_files.Items.Add(e.UserState as String);
+            FindMyString(e.UserState as String);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            if (BackgroundWorker.IsBusy)
+            {
+                BackgroundWorker.CancelAsync();
+            }
+        }
+
+        private void FindMyString(string searchString)
+        {
+            if (searchString != string.Empty)
+            {
+                int index = list_files.FindString(searchString);
+                if (index != -1)
+                {
+                    list_files.SetSelected(index, true);
+                }
+            }
+        }
+    }
+}
